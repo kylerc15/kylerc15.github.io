7ac1cac0c27ad8fc112f2dee0964201cab1051b2
kylerc15 <cam16040@byui.edu>
2021-02-13 21:02:26 -0700

    update

---
+++ lesson6/js/java.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', ()=>{
+    const hambutton = document.querySelector('.hamButton');
+    const mainnav = document.querySelector('#navigation');
+
+    hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')}, false);
+
+
+
+const today = new Date();
+const year = today.getFullYear();
+const date = today.getDate();
+const day = today.getDay();
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const monthName = months[today.getMonth()];
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayName = days[today.getDay()];
+
+const formatDate = `${dayName}, ${date} ${monthName} ${year}`;
+document.getElementById('curDate').innerHTML = formatDate.toLocaleString();
+
+if(today.getDay() == 5) {
+    document.getElementById('pancake').style.display = 'block';
+}
+}
+,);
