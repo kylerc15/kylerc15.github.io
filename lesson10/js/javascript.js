817a48e4bd202d46f9991a4aa601500287e7edd4
kylerc15 <cam16040@byui.edu>
2021-03-15 02:35:24 -0600

    lesson10

---
+++ lesson10/js/javascript.js
@@ -0,0 +1,97 @@
+window.addEventListener('load', ()=>{
+    const hambutton = document.querySelector('.hamButton');
+    const mainnav = document.querySelector('#navigation');
+
+    hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')}, false);
+
+
+
+const today = new Date();
+const year = today.getFullYear();
+const date = today.getDate();
+const day = today.getDay();
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const monthName = months[today.getMonth()];
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayName = days[today.getDay()];
+
+const formatDate = `${dayName}, ${date} ${monthName} ${year}`;
+document.getElementById('curDate').innerHTML = formatDate.toLocaleString();
+
+if(today.getDay() == 5) {
+    document.getElementById('pancake').style.display = 'block';
+}
+
+const requestURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=4bc5043b865ab98443e7950c385bf8e1&units=imperial';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+
+    const weather = jsonObject['weather'];
+
+    const current = document.getElementById('current');
+    current.innerHTML = weather[0].main;
+
+    const main = jsonObject['main'];
+    const high = document.getElementById('temp');
+    high.innerHTML = main.temp_max;
+
+    const humid = document.getElementById('humid');
+    humid.innerHTML = main.humidity;
+
+    const wind = jsonObject['wind'];
+    const wspeed = document.getElementById('wSpeed');
+    wspeed.innerHTML = wind.speed;
+
+},);
+});
+
+const requestURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&cnt=5&appid=4bc5043b865ab98443e7950c385bf8e1&units=imperial';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+
+    const forecast = jsonObject['list'];
+
+    for (let i = 0; i < forecast.length; i++ ) {
+
+        const temp = document.getElementById(i);
+        temp.innerHTML = forecast[i].main.temp;
+    }
+
+    for (let i = 0; i < forecast.length; i++) {
+
+        const iNum = i + 10
+        const image = document.getElementById(iNum);
+        const url = forecast[i].weather[0].icon;
+        image.setAttribute('src', "http://openweathermap.org/img/wn/" + url + "@2x.png");
+
+    }
+
+const today = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayName = days[today.getDay()];
+
+    for (let i = 4; i < 9; i++) {
+        
+        const curDay = document.getElementById(i+1);
+
+        const dayTest = today.getDay();
+
+        const dayNum = dayTest + (i-3);
+
+        curDay.innerHTML = days[dayNum].substr(0, 3);
+    }
+});
--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,18 +1,34 @@
 window.addEventListener('load', ()=>{
 
-const temp = document.getElementById("temp").innerHTML;
-const wSpeed = document.getElementById("wSpeed").innerHTML;
+    const requestURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=4bc5043b865ab98443e7950c385bf8e1&units=imperial';
 
-console.log(temp);
-console.log(wSpeed);
+    fetch(requestURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonObject) {
+    
+        // console.table(jsonObject);  // temporary checking for valid response and data parsing
+        
+        const main = jsonObject['main'];
 
-if(temp<=50 && wSpeed>3){
+        const temp = main.temp_max;
 
-    const wChill = 35.74 + (0.6215*temp) - (35.75*(Math.pow(wSpeed,0.16))) + ((0.4275*temp)*(Math.pow(wSpeed,0.16)));
+        const wind = jsonObject['wind'];
+        const wSpeed = wind.speed;
 
-    document.getElementById("wChill").innerHTML = Math.round(wChill);
-}
-else {
-    document.getElementById("allChill").innerHTML = "N/A";
-}
-});
+        // console.log(temp);
+        // console.log(wSpeed);
+
+        if(temp<=50 && wSpeed>3){
+
+            const wChill = 35.74 + (0.6215*temp) - (35.75*(Math.pow(wSpeed,0.16))) + ((0.4275*temp)*(Math.pow(wSpeed,0.16)));
+        
+            document.getElementById("wChill").innerHTML = Math.round(wChill);
+        }
+        else {
+            document.getElementById("allChill").innerHTML = "N/A";
+        }
+        });
+    
+    },);
